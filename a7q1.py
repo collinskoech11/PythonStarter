80ff174877aa4374295e6d5af89dd65fe8ba33b5
collinskoech11 <collinskoechck34@gmail.com>
2021-03-11 09:58:09 +0300

    update

--- a7q1.py
+++ a7q1.py
@@ -23,27 +23,17 @@ quizResults = [
     [0, 0, 1, 1, 1, 1, 0, 1, 0, 0],
     [0, 0, 1, 1, 0, 1, 0, 1, 0, 1]
 ]
-
 #reshape array
 reshapeArr=np.array(quizResults).reshape(-1,2)
-
 print(x)
-
-
 #map excecutable array
 data = np.array(x) 
-
 headers = ["0,0","0,1","1,0","1,1"]
 #tabulate the data
 table = tabulate(data, headers, tablefmt="github")
-
 print(table)
-
-
 #calculate percentage 
 values = set([y for row in reshapeArr for y in row])
 print([[(a==x).sum()*100.0/len(a) for x in values] for a in reshapeArr],'percent solved question 1')
-
 print([[(a==x).sum()*100.0/len(a) for x in values] for a in reshapeArr],'percent solved question 2')
-
 print([[(a==x).sum()*100.0/len(a) for x in values] for a in reshapeArr],'percent solved question 7')
--- a7q2.py
+++ a7q2.py
@@ -1,25 +1,18 @@
 import numpy as np
-
 #read data from dataset
 datafife = open("provincial_seats.txt", "r")
 lines = datafile.readlines()
-
 #strip data to one line
 mylistdata = [line.rstrip('\n') for line in datafile]
-
 #convert data into an array
 myarray = np.array(mylistdata)
-
 print(myarray)
-
 #reshape array
 reshapeMyArr = np.reshape(myarray, (-1,3))
 # newArr = arr.reshape(3, 13)
 print(reshapeMyArr)
-
 #Calculate the Total Population
 test_list = [reshapeMyArr]
-
 res_list = [] 
 for i in range(0, len(test_list)) : 
     if test_list[i] == 3 : 
