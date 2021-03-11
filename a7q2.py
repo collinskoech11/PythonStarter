fa24f4ab9e77119a95e286069a2df23821c25f85
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2021-03-11 06:17:35 +0300

    Update a7q2.py

--- a7q2.py
+++ a7q2.py
@@ -1,13 +1,18 @@
 import numpy as np
 
+#read data from dataset
 f = open("provincial_seats.txt", "r")
 lines = f.readlines()
+
+#strip data to one line
 mylist = [line.rstrip('\n') for line in f]
 
+#convert data into an array
 myarray = np.array(mylist)
 
 print(myarray)
 
+#reshape array
 reshapeMyArr = np.reshape(myarray, (-1,3))
 # newArr = arr.reshape(3, 13)
 print(reshapeMyArr)
